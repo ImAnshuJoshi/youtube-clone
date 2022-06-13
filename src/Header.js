5a3290016b86a7d12e001eeb8df6b730a1375bf8
Anshu <anshu.aj.joshi@gmail.com>
2022-06-13 06:30:41 +0530

    material ui imported

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';  
+
+function Header() {
+  return (
+    <div>
+      <h1>I am a header</h1>
+      <MenuIcon/>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" alt="" />
+    </div>
+  )
+}
+
+export default Header
