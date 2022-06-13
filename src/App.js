75c9b392e31bfb7cc6ee82d6526890b9de12d5fe
Anshu <anshu.aj.joshi@gmail.com>
2022-06-14 00:17:18 +0530

    nav bar updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Header.js';
+// import './Sidebar'
 
 function App() {
   return (
     <div className="App">
      <Header />
+     {/* <div className="app__page">
+       <sidebar/>
+     </div> */}
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
 import MenuIcon from '@mui/icons-material/Menu';  
+import SearchIcon from '@mui/icons-material/Search';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Header() {
   return (
-    <div>
-      <h1>I am a header</h1>
-      <MenuIcon/>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" alt="" />
+    <div className="header">
+      <div className="header__left">
+      <MenuIcon className="burger"/>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" alt="" className='header__logo'/>
+      </div>
+
+      <div className="header__input">
+      <input type="text" placeholder="Search" />
+      <SearchIcon className="header__inputButton" />
+      </div>
+
+      <div className="header__icons">
+      <VideoCallIcon/>
+      <AppsIcon/>
+      <NotificationsIcon/>
+      <AccountCircleIcon/>
+      </div>
+      
     </div>
   )
 }
