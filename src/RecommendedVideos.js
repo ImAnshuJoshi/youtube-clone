3f1282510e6dac857c3ab16f978795b89391a932
Anshu <anshu.aj.joshi@gmail.com>
2022-06-14 20:48:13 +0530

    videos added

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,9 +1,73 @@
 import React from 'react';
 import './RecommendedVideos.css'
+import VideoCard from './VideoCard';
+import './VideoCard.css';
+import './VideoCard.css'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+        title="this is my first video"
+        views="113 Views"
+        timestamp="3 days ago"
+        channelImage={AccountCircleIcon}
+        channel="Anshu Joshi"
+        image ='./images/1.jpg'
+        />
+        <VideoCard
+        title="this is my first video"
+        views="113 Views"
+        timestamp="3 days ago"
+        channelImage={AccountCircleIcon}
+        channel="Anshu Joshi"
+        image ="./images/1.jpg"
+        />
+         <VideoCard
+        title="this is my first video"
+        views="113 Views"
+        timestamp="3 days ago"
+        channelImage={AccountCircleIcon}
+        channel="Anshu Joshi"
+        image ="./images/1.jpg"
+        />
+        <VideoCard
+        title="this is my first video"
+        views="113 Views"
+        timestamp="3 days ago"
+        channelImage={AccountCircleIcon}
+        channel="Anshu Joshi"
+        image ="./images/1.jpg"
+        />
+        <VideoCard
+        title="this is my first video"
+        views="113 Views"
+        timestamp="3 days ago"
+        channelImage={AccountCircleIcon}
+        channel="Anshu Joshi"
+        image ="./images/1.jpg"
+        />
+        <VideoCard
+        title="this is my first video"
+        views="113 Views"
+        timestamp="3 days ago"
+        channelImage={AccountCircleIcon}
+        channel="Anshu Joshi"
+        image ="./images/1.jpg"
+        />
+        <VideoCard
+        title="this is my first video"
+        views="113 Views"
+        timestamp="3 days ago"
+        channelImage={AccountCircleIcon}
+        channel="Anshu Joshi"
+        image ="./images/1.jpg"
+        />
+
+      </div>
     </div>
   )
 }
---
+++ src/VideoCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import './VideoCard.css';
+
+function VideoCard({image,title,channel,timestamp,views}) {
+  return (
+    <div className="videoCard">
+      <img className ="videoCard__thumbnail" src="{image}" alt="" />
+        <div className="videoCard__info">
+            <AccountCircleIcon className="videoCard__avatar" alt={channel}/>
+
+        </div><div className="video__info">
+            <h4>{title}</h4>
+            <p>{channel}</p>
+              <p>{views}*{timestamp}</p>
+        </div>
+    </div>
+  )
+}
+
+export default VideoCard
