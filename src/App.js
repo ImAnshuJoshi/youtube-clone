6f64cab0a4f7868dc5991838a96c0ecec3cc1645
Anshu <anshu.aj.joshi@gmail.com>
2022-06-14 07:17:28 +0530

    sidebar completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
+import './Sidebar.css'
 import Header from './Header.js';
-// import './Sidebar'
+import Sidebar from './Sidebar';
+import RecommendedVideos from './RecommendedVideos';
 
 function App() {
   return (
     <div className="App">
      <Header />
-     {/* <div className="app__page">
-       <sidebar/>
-     </div> */}
+     <div className="app__page">
+       <Sidebar/>
+       <RecommendedVideos/>
+     </div>
     </div>
   );
 }
